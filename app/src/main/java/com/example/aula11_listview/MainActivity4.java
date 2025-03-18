cd2948600613d9b89be6a290b303aaa9e2c10b33
pedilha <pedrowilian13@gmail.com>
2025-03-17 23:32:34 -0300

    adição das 4 telas conectadas

--- app/src/main/java/com/example/aula11_listview/MainActivity4.java
+++ app/src/main/java/com/example/aula11_listview/MainActivity4.java
@@ -3,10 +3,9 @@ package com.example.aula11_listview;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListView;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,15 +15,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnprox,btnant;
+public class MainActivity4 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.tela4);
         ListView listView = (ListView) findViewById(R.id.lista);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -32,8 +29,9 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+
         Log.d("MainActivit","onCreate; Startes.");
-        ArrayList <String> nomes = new ArrayList<>();
+        ArrayList<String> nomes = new ArrayList<>();
         nomes.add("Pedro Willian Moraes Lourenço");
         nomes.add("Ana Clara Tomaz");
         nomes.add("Lucas de oliveira");
@@ -42,14 +40,12 @@ public class MainActivity extends AppCompatActivity {
         nomes.add("Geraldo Ribeiro");
         nomes.add("nome teste final");
 
-        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.layout_lista1, nomes);
+        ArrayAdapter adapter = new ArrayAdapter(this, R.layout.layout_lista4, nomes);
         listView.setAdapter(adapter);
-
     }
-    public void avancar (View v){
-        Intent intent = new Intent(v.getContext(), MainActivity2.class);
+
+    public void voltar (View v){
+        Intent intent = new Intent(v.getContext(), MainActivity3.class);
         v.getContext().startActivity(intent);
     }
-
-
 }
